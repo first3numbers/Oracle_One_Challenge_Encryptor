c1526ac6928983fd052ad200e50975cfb4582e40
first3numbers <marcelohac2016@gmail.com>
2022-01-20 15:45:18 -0300

    further developed JS logic.

--- js/encrypt.js
+++ js/encrypt.js
@@ -1,11 +1,23 @@
-function getValue() {
+
+function errorMessage() {
+    alert("Por favor, insira um texto")
+}
+
+function encryptText() {
+
     var originalText = document.getElementById("textInput").value;
-    var swapWords = originalText.replace(/e/g, "enter").replace(/i/g, "imes")
-    encryptedText = swapWords;
-    console.log(encryptedText)
+    var RawText = originalText
+
+    if (RawText == '' || null || 0) {
+        alert("Por favor, insira um texto")
+    } else {
+        var swapWords = RawText.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+        encryptedText = swapWords;
+
+        console.log(encryptedText)
+    }
 
 }
 
-
 var button = document.getElementById("btn-crypt")
-button.onclick = getValue;
+button.onclick = encryptText;
