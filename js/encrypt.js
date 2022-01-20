5d0b3a9fcdec174d8aa7bba603f0a09198005178
first3numbers <marcelohac2016@gmail.com>
2022-01-20 16:42:29 -0300

    JS logic base added. minor funcions + CSS to be added soon

--- js/encrypt.js
+++ js/encrypt.js
@@ -1,21 +1,23 @@
 
-function errorMessage() {
-    alert("Por favor, insira um texto")
-}
 
 function encryptText() {
 
-    var originalText = document.getElementById("textInput").value;
-    var RawText = originalText
+    var fetchText = document.getElementById("textInput").value;
+    var rawText = fetchText
+    var lowercaseText = rawText.toLowerCase();
+    var finalEncryptedOutput = document.getElementById("encryptOutput")
 
-    if (RawText == '' || null || 0) {
-        alert("Por favor, insira um texto")
+    if (lowercaseText == '' || null || 0) {
+        alert("Please insert a text.")
     } else {
-        var swapWords = RawText.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+        var swapWords = lowercaseText.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
         encryptedText = swapWords;
 
         console.log(encryptedText)
     }
+
+    finalEncryptedOutput.value = encryptedText
+
 
 }
 
